f1a89822292955dc389e6a88ffc5f93d24635702
sldx12 <63618062+sldx12@users.noreply.github.com>
2023-02-25 21:25:02 +0000

    fix

--- TPC2/main.py
+++ TPC2/main.py
@@ -5,19 +5,24 @@ def main():
 	print("Introduza o texto.")
 	text = input("> ")
 
+	tmp_str = "0"
+
 	i = 0
 	while i < len(text):
 		if text[i:i+3].lower() == "off":
 			summing = False
+			sum += int(tmp_str)
+			tmp_str = "0"
 			i += 3
 		elif text[i:i+2].lower() == "on":
 			summing = True
 			i += 2
 		elif text[i] == "=":
+			sum += int(tmp_str)
 			print(sum)
 			i += 1
 		elif summing == True:
-			sum += int(text[i])
+			tmp_str += text[i]
 			i += 1
 		elif summing == False:
 			i += 1
