f6e3a1b244f700d0cbd77ddee08b04168bc433fa
sldx12 <63618062+sldx12@users.noreply.github.com>
2023-03-09 14:14:10 +0000

    parte 2

--- TPC4/main.py
+++ TPC4/main.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def main():
 	while True:
@@ -26,6 +27,44 @@ def main():
 					ii += 1
 				final.append(tmp)
 			
+			y = json.dumps(final)
+
+			with open('output.json', 'w') as j:
+				j.write(y)
+
+		else:
+			colunas = cabeca.split(",")
+			colunas = colunas[:-1]
+
+			final = []
+			tmp = {}
+
+			for linha in linhas:
+				cols = linha.split(",")
+				cols[-1] = cols[-1].rstrip("\n")
+				ii = 0
+				print(colunas)
+				cc = 0
+				while cc < len(colunas):
+					if "{" not in colunas[cc]:
+						tmp[colunas[cc]] = cols[ii]
+						ii += 1
+						cc += 1
+					else:
+						print(cols[ii])
+						num = re.search(r"\{(.+?)\}", colunas[cc])
+						num = int(num.group(1))
+						content = re.search(r"(.+?)\{", colunas[cc])
+						content = content.group(1)
+
+						tmp[content] = []
+						for x in range(0, num):
+							tmp[content].append(cols[ii])
+							ii += 1
+							cc += 1
+
+				final.append(tmp)
+
 			y = json.dumps(final)
 
 			with open('output.json', 'w') as j:
