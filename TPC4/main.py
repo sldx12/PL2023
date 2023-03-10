b517fbf593bdfe41583613a36501990b9018b4fd
sldx12 <63618062+sldx12@users.noreply.github.com>
2023-03-10 11:47:47 +0000

    finished

--- TPC4/main.py
+++ TPC4/main.py
@@ -70,7 +70,7 @@ def main():
 			with open(o_filename, 'w') as j:
 				j.write(y)
 
-		elif re.search(r'\{(\d+(,\d+)*)\}', cabeca) != None:
+		elif re.search(r'\{(\d+(,\d+)*)\}', cabeca) != None and "::" not in cabeca:
 			colunas = []
 			temp = ""
 			skip_next = False
@@ -127,6 +127,78 @@ def main():
 									tmp[content].append(cols[ii])
 									ii += 1
 									cc += 1
+
+				final.append(tmp)
+
+			y = json.dumps(final)
+			
+			o_filename = filename.split(".")[0] + ".json"
+			with open(o_filename, 'w') as j:
+				j.write(y)
+
+		elif "::" in cabeca:
+			colunas = []
+			temp = ""
+			skip_next = False
+
+			for i, c in enumerate(cabeca):
+				if skip_next:
+					temp += c
+					if c == "}":
+						skip_next = False
+					continue
+				elif c == '{':
+					skip_next = True
+					temp += c
+				elif c == ',':
+					if skip_next:
+						temp += c
+					else:
+						colunas.append(temp)
+						temp = ""
+				else:
+					temp += c
+
+			colunas.append(temp)
+
+			colunas.pop()
+			colunas.pop()
+
+			final = []
+
+			for linha in linhas:
+				tmp = {}
+				cols = linha.split(",")
+				ii = 0
+				cc = 0
+				while cc < len(colunas) - 1:
+					if "{" not in colunas[cc]:
+						tmp[colunas[cc]] = cols[ii]
+						ii += 1
+						cc += 1
+					else:
+						match = re.search(r'(\d+),(\d+)', colunas[cc])
+						num_min = int(match.group(1))
+						num_max = int(match.group(2))
+						content = re.search(r"(.+?)\{", colunas[cc])
+						content = content.group(1)
+						content2 = re.search(r"::(.+)", colunas[cc])
+						content2 = content2.group(1)
+
+						tmp2 = []
+						for x in range(0, num_max):
+							if x < num_min:
+								tmp2.append(int(cols[ii]))
+								ii += 1
+								cc += 1
+							else:
+								if len(cols[ii]) != 0 and cols[ii] != '\n':
+									tmp2.append(int(cols[ii]))
+									ii += 1
+									cc += 1
+
+						n = content + "_" + content2
+						tmp[n] = sum(tmp2)
 
 				final.append(tmp)
 
